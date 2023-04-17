fe78f71e5fdd13bd980402fbd3845e66dd743a1e
Johannamorel2093 <109986562+Johannamorel2093@users.noreply.github.com>
2023-04-17 14:08:02 -0300

    Add files via upload

---
+++ proyecto1/js/logica.js
@@ -0,0 +1,49 @@
+
+$(function() {
+
+  $("form[name='registration']").validate({
+
+    rules: {
+
+      firstname: "required",
+
+      lastname: "required",
+
+      email: {
+
+        required: true,
+        email: true
+
+      },
+
+      password: {
+        required: true,
+        minlength: 5
+
+      }
+
+    },
+       messages: {
+        firstname: "Por favor, introduzca su nombre",
+        lastname: "Por favor, introduzca su apellido",
+        password: {
+        required: "Por favor proporcione una contraseña",
+        minlength: "Su contraseña debe tener al menos 5 caracteres."
+ },
+
+       email: "Por favor, introduce una dirección de correo electrónico válida"
+
+    },
+
+    submitHandler: function(form) {
+
+      form.submit();
+     alert ('Registrado con éxito.'); 
+    }
+    
+   
+
+  });
+
+});
+
